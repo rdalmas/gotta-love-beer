bccd7cc4e236c04b9e6d3874fa0cf25cc1fc0d04
Rodrigo Dalmas <rodrigo.dalmas.external@zalando.de>
2019-12-18 10:19:09 +0100

    Using Card component instead on the beer component

--- src/components/Beer/Beer.jsx
+++ src/components/Beer/Beer.jsx
@@ -1,22 +1,54 @@
 import React from "react";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
-const Beer = ({ beer }) => (
-  <GridListTile key={beer.id}>
-    <img src={beer.image_url} alt={beer.name} style={{ height: 250 }} />
-    <GridListTileBar
-      title={beer.name}
-      subtitle={<span>More info: {beer.tagline}</span>}
-      actionIcon={
-        <IconButton aria-label={`info about ${beer.name}`}>
-          <InfoIcon />
+const useStyles = makeStyles({
+  card: {
+    width: 200,
+    height: "auto"
+  },
+  media: {
+    height: 200
+  },
+});
+
+const Beer = ({ beer }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={beer.image_url}
+          title={beer.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" aria-label="beer name">
+            {beer.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p" aria-label="beer tagline">
+            {beer.tagline}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
         </IconButton>
-      }
-    />
-  </GridListTile>
-)
+        <Button size="small" color="primary" arial-label="more info">
+          More info
+        </Button>
+      </CardActions>
+    </Card>
+  )
+};
 
 export default Beer;
